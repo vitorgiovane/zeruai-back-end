5ca6c41e5d28181a3d1bf77d588863c3d8925287
Vitor Giovane <vitorgpalves@gmail.com>
2020-06-07 21:05:00 -0300

    feature: adiciona verificação de atributos às rotas de criação de sessão e usuários

--- src/routes/sessions.ts
+++ src/routes/sessions.ts
@@ -2,10 +2,16 @@ import { Router } from 'express'
 
 import AuthenticateUserService from '../services/AuthenticateUser'
 
+import AppError from '../errors/AppError'
+
 const sessionsRouter = Router()
 
 sessionsRouter.post('/', async (request, response) => {
   const { email, password } = request.body
+
+  if (!email || !password) {
+    throw new AppError('The attributes email and password are required.')
+  }
 
   const authenticateUser = new AuthenticateUserService()
   const { user, token } = await authenticateUser.run({ email, password })
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express'
 
 import CreateUserService from '../services/CreateUser'
+import AppError from '../errors/AppError'
 
 const usersRouter = Router()
 
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body
+
+  if (!name || !email || !password) {
+    throw new AppError('The attributes email, name and password are required.')
+  }
+
   const createUser = new CreateUserService()
   const user = await createUser.run({ name, email, password })
   delete user.password
