ccc317a021949038f5b4860810327b4b09ebfe61
Vitor Giovane <vitorgpalves@gmail.com>
2020-06-13 16:25:54 -0300

    feature: habilita cors globalmente

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata'
 
 import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
+
 import 'express-async-errors'
 
 import routes from './routes'
@@ -10,6 +12,7 @@ import './database'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
